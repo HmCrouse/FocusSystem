91665041f2f86c3ca45a6583408917566894e3b2
Hayley Crouse <hayleycrouse@developersinstitute.co.nz>
2022-07-04 11:20:17 +1200

    create react app as well as header component and folders for component and views

---
+++ focus-system/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// Components
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
