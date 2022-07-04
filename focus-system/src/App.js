249d746bd23e6f63a9ea3b8e6b99b7acf979033c
Hayley Crouse <hayleycrouse@developersinstitute.co.nz>
2022-07-04 12:40:03 +1200

    add tasklist to app.js to render tasklist on browser

--- focus-system/src/App.js
+++ focus-system/src/App.js
@@ -1,12 +1,19 @@
+import { React, useState } from "react";
+
 import "./App.css";
+
+import data from "./data/mockData.json";
 
 // Components
 import Header from "./components/header";
+import TaskList from "./components/tasklist";
 
 function App() {
+  const [taskList, setTaskList] = useState(data);
   return (
     <div className="App">
       <Header />
+      <TaskList taskList={taskList} />
     </div>
   );
 }
